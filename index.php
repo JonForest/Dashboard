4ea7a615079a40e36db0491988484deba65ef7cb
Jon Hollingsworth <jonforest@gmail.com>
2013-09-29 21:47:42 +1300

    Test commit

--- index.php
+++ index.php
@@ -31,7 +31,8 @@
         <script type="text/template" id="items-template">
             <button class="btn btn-add">Add</button>
         </script>
-        
+
+        <script sfsdfd>
         
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
