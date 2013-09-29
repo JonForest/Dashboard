91acbb6faf3c609a20333bc239b210959b77e897
Jon Hollingsworth <jonforest@gmail.com>
2013-09-29 20:36:09 +1300

    Test commit

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
         
         
         <script type="text/template" id="task-template">
+            This is a test
         </script>
         
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
